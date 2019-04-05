05504a03be102f2eba4ca90cd07a8df7cf7acdef
Asda <omega-faworit@yandex.ru>
2019-04-05 20:42:25 +0300

    Added finish class

--- GeneticAlgV0.2/src/Objects/Finish.java
+++ GeneticAlgV0.2/src/Objects/Finish.java
@@ -6,7 +6,12 @@ public class Finish extends Obstacle {
 
 	public Finish(ModelEntity[] obstacles, float chunkLength) {
 		super(obstacles, chunkLength);
-		
+	}
+	
+	public void finishCheck(Car car) {
+		if(super.detectCollision(car)==true) {
+			System.out.println("GJ!");
+		}
 	}
 	
 }
--- GeneticAlgV0.2/src/main/Main.java
+++ GeneticAlgV0.2/src/main/Main.java
@@ -11,6 +11,7 @@ import engine.rendering.models.ModelEntity;
 import engine.rendering.models.TexturedModel;
 import engine.shaders.BasicShader;
 import objects.Car;
+import objects.Finish;
 import objects.Obstacle;
 
 public class Main {
@@ -30,16 +31,26 @@ public class Main {
 		TexturedModel car = Loader.loadModel("Car.obj", "Car4.png");
 		TexturedModel floor = Loader.loadModel("floor.obj", "floor.png");
 		TexturedModel brick = Loader.loadModel("brick.obj", "brick.png");
+		TexturedModel finish = Loader.loadModel("finish.obj", "finish.png");
 		Car carEntity = new Car(car, new Vector3f(0, 0, 0f), new Vector3f(0f, 0f, 0f), new Vector3f(1f, 1f, 1f), 0.001f);
 		ModelEntity floorEntity = new ModelEntity(floor, new Vector3f(0f, -1f, 0f), new Vector3f(0, 0, 0), new Vector3f(1f, 1f, 1f));
-		ModelEntity brickEntity1 = new ModelEntity(brick, new Vector3f(1f, 0f, 3f), new Vector3f(0, 0, 0), new Vector3f(1f, 1f, 1f));
-		ModelEntity brickEntity2 = new ModelEntity(brick, new Vector3f(5f, 0f, 10f), new Vector3f(0, 0, 0), new Vector3f(1f, 1f, 1f));
-		ModelEntity brickEntity3 = new ModelEntity(brick, new Vector3f(10f, 0f, 7f), new Vector3f(0, 0, 0), new Vector3f(1f, 1f, 1f));
-		Obstacle obstacles = new Obstacle(new ModelEntity[]{brickEntity1, brickEntity2, brickEntity3}, 10f);
+		ModelEntity finishEntity = new ModelEntity(finish, new Vector3f(5f, 0f, 10f), new Vector3f(0, 0, 0), new Vector3f(1f, 1f, 1f));
+		ModelEntity brickEntity1 = new ModelEntity(brick, new Vector3f(0.8f, 0f, 0f), new Vector3f(0, 0, 0), new Vector3f(1f, 1f, 1f));
+		ModelEntity brickEntity2 = new ModelEntity(brick, new Vector3f(-0.8f, 0f, 0f), new Vector3f(0, 0, 0), new Vector3f(1f, 1f, 1f));
+		ModelEntity brickEntity3 = new ModelEntity(brick, new Vector3f(0.8f, 0f, 1.2f), new Vector3f(0, 0, 0), new Vector3f(1f, 1f, 1f));
+		ModelEntity brickEntity4 = new ModelEntity(brick, new Vector3f(-0.8f, 0f, 1.2f), new Vector3f(0, 0, 0), new Vector3f(1f, 1f, 1f));
+		ModelEntity brickEntity5 = new ModelEntity(brick, new Vector3f(-0.5f, 0f, 2.35f), new Vector3f(0, -30f, 0), new Vector3f(1f, 1f, 1f));
+		ModelEntity brickEntity6 = new ModelEntity(brick, new Vector3f(1.1f, 0f, 2.28f), new Vector3f(0, -30f, 0), new Vector3f(1f, 1f, 1f));
+		ModelEntity brickEntity7 = new ModelEntity(brick, new Vector3f(0f, 0f, 3.5f), new Vector3f(0, -30, 0), new Vector3f(1f, 1f, 1f));
+		ModelEntity brickEntity8 = new ModelEntity(brick, new Vector3f(10f, 0f, 7f), new Vector3f(0, -30, 0), new Vector3f(1f, 1f, 1f));
+		Obstacle obstacles = new Obstacle(new ModelEntity[]{brickEntity1, brickEntity2, brickEntity3, brickEntity4, brickEntity5,brickEntity6, brickEntity7, brickEntity8}, 10f);
+		Finish finishes = new Finish(new ModelEntity[]{finishEntity}, 10f);
 		renderer.proseeEntity(carEntity);
-		renderer.proseeEntity(carEntity.box);
+		//renderer.proseeEntity(carEntity.box);
 		renderer.proseeEntity(floorEntity);
+		renderer.proseeEntity(finishEntity);
 		obstacles.render(renderer);
+		
 		while (!window.closed()) {
 			if (window.isUpdating()) {
 				keyActions(carEntity, obstacles);
@@ -48,6 +59,7 @@ public class Main {
 				camera.update(window);
 				carEntity.update();
 				renderer.loadCamera(camera);
+				finishes.finishCheck(carEntity);
 				shader.bind();
 				shader.useMatrices();
 				renderer.render();
@@ -55,7 +67,7 @@ public class Main {
 				window.swapBuffers();
 			}
 		}
-
+		finish.remove();
 		car.remove();
 		floor.remove();
 		brick.remove();
